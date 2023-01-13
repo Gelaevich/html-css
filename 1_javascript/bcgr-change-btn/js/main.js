5ec14de02e28da47c8d530d5fa508fd66dcbe374
Heorhiy Dokvadze <gdok92@gmail.com>
2023-01-13 17:46:07 +0200

    remove async, optimize js

--- 1_javascript/bcgr-change-btn/js/main.js
+++ 1_javascript/bcgr-change-btn/js/main.js
@@ -1,14 +1,11 @@
-setInterval(function () {
-  (firstNum = Math.floor(Math.random() * 256)),
-    (secondNum = Math.floor(Math.random() * 256)),
-    (thirdNum = Math.floor(Math.random() * 256));
-}, 300);
-
 let firstNum, secondNum, thirdNum;
 let wrapper = document.getElementById("wrapper");
 let currentColor = document.getElementById("currColor");
 
 function dataOutput() {
-  (wrapper.style.backgroundColor = `rgb(${firstNum}, ${secondNum}, ${thirdNum})`),
+  (firstNum = Math.floor(Math.random() * 256)),
+    (secondNum = Math.floor(Math.random() * 256)),
+    (thirdNum = Math.floor(Math.random() * 256)),
+    (wrapper.style.backgroundColor = `rgb(${firstNum}, ${secondNum}, ${thirdNum})`),
     (currentColor.textContent = `rgb (${firstNum}, ${secondNum}, ${thirdNum})`);
 }
